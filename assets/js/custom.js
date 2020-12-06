b8c4cc090a50cdae77a85e058d72786ff3b6a16c
OmarAliSoliman <omarasmar9955@gmail.com>
2020-12-06 11:20:51 +0200

    cheack date seorator inview or not

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,31 +9,44 @@ $(document).ready(function () {
   //   }
   // })
 
+  $("#floating-bottom").click(() => {
+    $(".prog-sidebar").animate({ scrollTop: $(".prog-sidebar").height() }, 50);
+    $("#floating-top").css("display", "block");
+    $("#floating-bottom").css("display", "none");
+  });
 
-  $('#floating-bottom').click(()=>{
-    $('.prog-sidebar').animate({ scrollTop: $('.prog-sidebar').height() }, 50);
-    $('#floating-top').css('display', 'block');
-    $('#floating-bottom').css('display', 'none');
-  })
+  $("#floating-top").click(() => {
+    $(".prog-sidebar").animate({ scrollTop: 0 }, 50);
+    $("#floating-bottom").css("display", "block");
+    $("#floating-top").css("display", "none");
+  });
 
-  $('#floating-top').click(()=>{
-    $('.prog-sidebar').animate({ scrollTop: 0 }, 50);
-    $('#floating-bottom').css('display', 'block');
-    $('#floating-top').css('display', 'none');
-  })
+  // $(".fixed-date").on("inview", function (event, isInView) {
+  //   if (isInView) {
+  //     console.log("div is in view");
+  //   } else {
+  //     console.log("div is not in view");
+  //   }
+  // });
 
+  $(".date-divider").map(function (index, item) {
+    $(item).on("inview", function (event, isInView) {
+      if (isInView) {
+        // console.log("div is in view");
+        $(".fixed-date").fadeOut(1000);
+      } else {
+        $(".fixed-date").fadeIn("fast");
+      }
+    });
+  });
 
   // console.log($('.date-divider').last().offset().top)
-  $('.chat-body').scroll(function(){
-    $('.date-divider').map(function(index, item){
-      // console.log($(item).offset().top)
-      if($('.fixed-date').offset().top >= $(item).offset().top){
-        // console.log("true");
-        // console.log($(item).text());
-        $('.fixed-date').text($(item).text());
-      }else{
-        // console.log("false")
+  $(".chat-body").scroll(function () {
+    $(".date-divider").map(function (index, item) {
+      if ($(".fixed-date").offset().top >= $(item).offset().top) {
+        $(".fixed-date").text($(item).text());
+      } else {
       }
-    })
-  })
+    });
+  });
 });
